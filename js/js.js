da78dcd4f70f108de90ab38af0c332a2ad5cf8a5
Ryww0 <laurent.chaitas@gmail.com>
2022-05-27 09:53:35 +0200

    création nouvelle fonction JS pour afficher le txt 'télécharger mon cv'

--- js/js.js
+++ js/js.js
@@ -1,3 +1,7 @@
 function chbg(color) {
     document.getElementById('me-content').style.backgroundColor = color;
-}   
+}
+
+function dspl(display) {
+    document.getElementById('dlcv').style.display = display;
+}
