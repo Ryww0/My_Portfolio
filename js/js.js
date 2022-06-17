5cded707817eead5da313eb4423e3e362d344060
Ryww0 <laurent.chaitas@gmail.com>
2022-06-17 10:09:14 +0200

    rangement + commentaires js

--- js/js.js
+++ js/js.js
@@ -1,37 +1,42 @@
+// hover DLCV
+let txt = document.getElementById("dlcv");
+// Hover portfolio
+let newStyle = document.querySelectorAll(".portfolio-content");
+let empty = document.querySelectorAll(".empty-block");
+// Menu Burger
+const burger = document.querySelector(".menu");
+const menu = document.querySelector(".menu-links");
+const navBg = document.querySelector("nav");
+// Switch changement de thème
+const checkbox = document.querySelector("button");
+let root = document.documentElement;
+
+// Hover DLCV
 function chbg(color) {
   document.getElementById("me-content").style.backgroundColor = color;
 }
-
 function show() {
-  let txt = document.getElementById("dlcv");
-
   if (txt) {
     txt.style.display = "inline-block";
     txt.translate = (-500, 0);
     txt.duration = 1200;
   }
 }
-
 function hide() {
-  let txt = document.getElementById("dlcv");
-
   if (txt) {
     txt.style.display = "none";
   }
 }
 
+// Hover compétences
 function chbgSK1(color) {
   document.getElementById("front").style.backgroundColor = color;
 }
-
 function chbgSK2(color) {
   document.getElementById("graphisme").style.backgroundColor = color;
 }
 
-// hover portfolio
-let newStyle = document.querySelectorAll(".portfolio-content");
-let empty = document.querySelectorAll(".empty-block");
-
+// Hover portfolio
 newStyle.forEach((div) => {
   div.addEventListener("mouseover", function (event) {
     div.classList.add("portfolio-content-hover");
@@ -42,18 +47,13 @@ empty.forEach((div) => {
     div.classList.add("portfolio-content-hover-nope");
   });
 });
-
 newStyle.forEach((div) => {
   div.addEventListener("mouseout", function (event) {
     div.classList.remove("portfolio-content-hover");
   });
 });
 
-// menu burger
-const burger = document.querySelector(".menu");
-const menu = document.querySelector(".menu-links");
-const navBg = document.querySelector("nav");
-
+// Menu burger
 burger.addEventListener("click", navMenu);
 
 function navMenu() {
@@ -62,10 +62,7 @@ function navMenu() {
   navBg.classList.toggle("active");
 }
 
-//
-const checkbox = document.querySelector("button");
-let root = document.documentElement;
-
+// Switch changement de thème
 checkbox.addEventListener("click", (event) => {
   if (checkbox.classList.contains("on")) {
     checkbox.setAttribute("aria-checked", "false");
@@ -78,10 +75,3 @@ checkbox.addEventListener("click", (event) => {
   }
   checkbox.classList.toggle("on");
 });
-
-//FIXME
-// checkbox.addEventListener("click", function () {
-//   if (root.style.getPropertyValue("--white", "#372703")) {
-//   } else {
-//   }
-// });
